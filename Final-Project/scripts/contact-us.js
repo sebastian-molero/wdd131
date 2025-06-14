c35a87fb8a716f0a85209987de505efed5c888b8
sebastian-molero <smoleronavarro@byupathway.edu>
2025-06-14 16:20:41 -0500

    week 06 updates

--- Final-Project/scripts/contact-us.js
+++ Final-Project/scripts/contact-us.js
@@ -91,3 +91,9 @@ experiencesList.forEach(experience => {
     experiences.appendChild(option)
 });
 
+const yearSpan = document.getElementById("currentyear");
+const lastModifiedParagraph = document.getElementById("lastModified");
+
+yearSpan.textContent = new Date().getFullYear();
+lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+
--- Final-Project/scripts/home.js
+++ Final-Project/scripts/home.js
@@ -5,3 +5,11 @@ hamburgerMenu.addEventListener("click", () => {
     hamburgerMenu.classList.toggle("open")
     nav.classList.toggle("active");
 })
+
+
+const yearSpan = document.getElementById("currentyear");
+const lastModifiedParagraph = document.getElementById("lastModified");
+
+yearSpan.textContent = new Date().getFullYear();
+lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+
